8e29795a8cd052a309dad68ce7507f7db7da9abf
Alexander Zhurba <a.zhurba0810@gmail.com>
2026-03-16 17:12:26 +0200

    bug

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,11 @@ app.use(cors(
 ));
 app.use("/api/photos",photoRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "/fronted/dist")))
-  app.get("*" , (req,res) => {
-    res.sendFile(path.resolve(__dirname,"fronted","dist","index.html"))
-  })
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
   
 
