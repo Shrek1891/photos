0cdd4cf2b7d770f544ca33bc26895e4fe2ab767b
Alexander Zhurba <a.zhurba0810@gmail.com>
2026-04-02 23:33:41 +0300

    foolish

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/store.ts";
 import { Provider as ReduxProvider } from "react-redux";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
